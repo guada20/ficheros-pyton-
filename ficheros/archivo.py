07d1db8ebba4cb828799a5a2aaa82a8204077bdf
guada20 <113446819+guada20@users.noreply.github.com>
2023-03-07 15:17:20 -0500

    Add files via upload

--- ficheros/archivo.py
+++ ficheros/archivo.py
@@ -8,7 +8,7 @@ with open("Archivo2.txt", "r") as  archivo:
     contenido = archivo.readlines()
 print(contenido)"""
 
-
+"""
 with open("Archivo3.txt", "w")as file:
     file.write("lenguajes de programacion :\nPHP\nPyton\nJavascript")
 with open("Archivo3.txt", "r")as file:
@@ -20,6 +20,21 @@ existe =os.path.isfile("Archivo3.txt")
 existe2=os.path.isfile("Archivo4.txt")
 print(existe)
 print(existe2)
-
-#cambiar el nombre a un archivo 
+"""
+#cambiar el nombre a un archivo
+"""
 os.rename("Archivo2.txt", "Archivo1.txt")
+#eliminar archivos"""
+"""
+os.remove("Archivo0.txt")"""
+#RECORRER EL ARCHIVO PARA COLOCAR ARTICULSO EN UNA LISTA Y LOS PRECIOS EN OTRA }
+with open("Archivo2.txt","r") as readfile:
+    lineas = readfile.readline()
+    articulos = []
+    precios = []
+    for c_linea in readfile:
+        renglon=c_linea.split("#")
+        articulos.append(renglon[0])
+        precios.append(renglon[1])
+print(articulos)
+print(precios)
